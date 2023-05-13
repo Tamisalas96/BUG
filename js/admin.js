3af446b90dc6bf7e735304ed9500e6872c99ca86
nicoleonard <nikolasleonard@gmail.com>
2023-05-13 17:38:43 -0300

    logica para guardar el objeto en una lista de videojuegos en el local storage

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,16 @@
 import Videojuego from "./classVideojuego.js"
 
 function crearVideojuego(){
-    let resumenErrores = resumenValidaciones(nombre.value,descripcion.value,categoria.value,precio.value,imagen.value,desarrollador.value,requisitos.value)
+    let resumenErrores = sumarioValidaciones(nombre.value,descripcion.value,categoria.value,precio.value,imagen.value,desarrollador.value,requisitos.value)
 
     if (resumenErrores.length === 0){
         let nuevoJuego = new Videojuego(nombre.value,descripcion.value,categoria.value,precio.value,imagen.value,desarrollador.value,requisitos.value)
+        console.log(nuevoJuego);
+    }else{
+        alert("todo mal")
     }
 
+    function guardarLocal() {
+        localStorage.setItem("listaVideojuegos", JSON.stringify(listaVideojuegos));
+      }
 }
