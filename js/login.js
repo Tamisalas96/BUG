2a011d96c2ee70f652dde83faa8b613b0dd13de4
emanuel ruiz <ruizemanuelok@gmail.com>
2023-05-15 02:48:34 -0300

    validacion bootstrap en login

---
+++ js/login.js
@@ -0,0 +1,13 @@
+(() => {
+      const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
